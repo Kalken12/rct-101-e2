b37efc6e42e8fb7ed3d14afdb55478a6627645b9
Kalyani Kenekar <82832370+Kalken12@users.noreply.github.com>
2022-05-30 17:09:43 +0530

    first

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import { extendTheme, ChakraProvider, styled, StylesProvider } from '@chakra-ui/react'
 
-const Product = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+import {
+   Text ,
+ Image ,
+   Box ,
+  Stack ,
+   Heading,
+   Tag,
+   TagLabel 
+}from '@chakra-ui/react'
+
+
+const Product = ({product}) => {
+
+  let {  title, category , gender, imageSrc, price} = product ;
+  console.log("title", title)
+
   return (
     <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
+      <Image data-cy="product-image" src={product.imageSrc} />
+      <Text data-cy="product-category">{product.category}</Text>
       <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
+        <TagLabel data-cy="product-gender">{product.gender}</TagLabel>
       </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
+      <Heading data-cy="product-title">{product.title}</Heading>
+      <Box data-cy="product-price">{product.price}</Box>
     </Stack>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,148 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
+import AddProduct from './AddProduct'
+import Product from './Product'
+import Pagination from './Pagination'
+import style from './Products.module.css'
+import axios from 'axios'
+import { extendTheme, ChakraProvider, styled, StylesProvider } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  useDisclosure, ModalHeader
+  , ModalCloseButton,
+  ModalBody,
+  FormControl
+  , FormLabel,
+  Input,
+  ModalFooter,
+  Grid,
+  
+} from '@chakra-ui/react'
+
+
+ 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [emdata, setEmData] = useState([])
+  const [form, setForm] = useState({
 
+
+    title: "",
+    category: "",
+    gender: "",
+    imageSrc: "",
+    price: ""
+
+  })
+  
+
+  const onchange = (e) => {
+    console.log("e", e)
+    let { name, value } = e.target
+    setForm({
+      ...form ,
+      [name]: value 
+    })
+     console.log(value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post("http://localhost:8080/products", {
+      title: form.title,
+      category: form.category,
+      gender: form.gender,
+      imageSrc: "",
+      price: form.price
+
+    }).then(res => {
+      console.log(res.data)
+    })
+  }
+ 
+  fetch(`http://localhost:8080/products`)
+.then((r)=>r.json())
+.then((d)=>{
+  console.log("ddd",d)
+  setEmData(d)
+})
+   
+    // setEmData(emdata)
+    // console.log("emd",emdata)
+
+
+  
+
+ 
+ 
+   
+  
+
+ 
   return (
-    <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
-    </Flex>
+    <Grid>
+   
+    
+
+        <Button className={style.btnmain} onClick={onOpen}>Add Product</Button>
+
+
+        <Modal isOpen={isOpen} onClose={onClose} className={style.modal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Title" onChange={onchange} name="title" value={form.title} />
+                <select onChange={onchange} name="category" value={form.category}>
+                  <option value="Shirt">Shirt</option>
+                  <option value="Pant">Pant</option>
+                  <option value="Jeans">Jeans</option>
+                </select>
+                <div>
+                  <label>Male</label>
+                  <input type="radio" name="gender" onChange={onchange} value={"male"} />
+                  <label>Female</label>
+                  <input type="radio" name="gender" onChange={onchange} value={"female"} />
+                  <label>Unisex</label>
+                  <input type="radio" name="gender" onChange={onchange} value={"unisex"} />
+                </div>
+                <input type="number" onChange={onchange} name="price" value={form.price} />
+                <input type="submit" />
+              </form>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant='ghost'>Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      
+
+   <Flex>
+    {
+      emdata.map((el)=>{
+    <Product key={el.id} product={el}  />
+
+    })
+  }
+</Flex>
+  
+   
+    
+    <Pagination/>
+    </Grid>
   );
 };
 
